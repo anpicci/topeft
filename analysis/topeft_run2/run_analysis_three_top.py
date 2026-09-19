b884f9cd32764ad95a409d88f29886cbcb4f8514
Andrea Piccinelli <andrea8395@gmail.com>
2026-09-19 11:36:23 -0400

    fix(topeft): default three-top runs to Work Queue port 9165

--- analysis/topeft_run2/run_analysis_three_top.py
+++ analysis/topeft_run2/run_analysis_three_top.py
@@ -1098,7 +1098,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--port",
-        default="9164-9170",
+        default="9165",
         help="Specify the Work Queue port. An integer PORT or an integer range PORT_MIN-PORT_MAX.",
     )
     parser.add_argument(
